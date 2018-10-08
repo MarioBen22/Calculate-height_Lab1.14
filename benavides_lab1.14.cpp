a5b3c4463e399817e115eaef60363e5527dcd92a
Mario Benavides Jr <36014007+MarioBen22@users.noreply.github.com>
2018-10-08 00:37:44 -0500

    Update benavides_lab1.14.cpp
    tidying up spacing and adding notes.

--- benavides_lab1.14.cpp
+++ benavides_lab1.14.cpp
@@ -1,15 +1,24 @@
 // This program calculates the height of a basketball player.
+// Mario Benavides
+// Lab 1.14 Completed
+
+
 #include <iostream>
 using namespace std;
+
 
 int main()
 {
 	double feet, inches;
 		
-	feet = 74 / 12; //calculate the feet.
+	feet = 74 / 12; 	//calculate the feet.
 	
-	inches = 74 % 12; //calculate the remaining inches.
+	inches = 74 % 12; 	//calculate the remaining inches.
 	
-	cout << "A 74 inch tall basketball player is " << feet << " feet " << inches << " inches tall." << endl; //Display the feet and inches.
+	
+				//Display the feet and inches.
+	cout << "A 74 inch tall basketball player is " << feet << " feet " << inches << " inches tall." << endl; 
+	
+	
 	return 0;
 }
